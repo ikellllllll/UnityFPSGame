78d465f5d9309bbe9abe699d6a20b425cc342ae2
Dori <qarkchanjin@gmail.com>
2023-11-07 11:09:39 +0900

    Charactor Moving
    캐릭터 Move 구현, 점프도 구현

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -3,17 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
-{
+{   
+    //플레이어 움직임 속도
     public float moveSpeed = 7f;
+    private CharacterController cc;
+    // 중력
+    private float gravity = -20f;
+    //수직 속력 변수
+    private float yVelocity = 0;
+
+    public float jumpPower = 10f;
+
+    public bool isJumping = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized;
+
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            if (isJumping)
+            {
+                isJumping = false;
+                yVelocity = 0;
+            }
+        }
         
+        //Space 키 누르면 점프
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            // 캐릭터 수직 속도에 점프력 적용
+            yVelocity = jumpPower;
+        }
+        //캐릭터 수직 속도에 중력 값을 적용
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+        // 이동 속도에 맞춰 이동
+        cc.Move(dir * moveSpeed * Time.deltaTime);
     }
 }
